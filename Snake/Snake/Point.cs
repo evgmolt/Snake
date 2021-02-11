97aaeb70748c632e2afc79ef808d13649c334d84
evgmolt <evg.molt@gmail.com>
2021-02-11 11:52:19 +0400

    Vertical and horizontal lines

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Point
+    {
+        int x;
+        int y;
+        char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public void Move(int dx, int dy)
+        {
+            x += dx;
+            y += dy;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+    }
+}
---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '*');
+            VerticalLine vline = new VerticalLine(5, 3, 18, '#');
+            line.Draw();
+            vline.Draw();
+            Console.ReadKey();
+        }
+    }
+}
