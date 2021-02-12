39f5d8d19efc82a49c1ce71586195b2d3b8109d6
evgmolt <evg.molt@gmail.com>
2021-02-12 10:59:25 +0400

    Game over

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,23 +11,11 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Console.SetBufferSize(80, 25);
-            }
-            catch (Exception)
-            {
+//            Console.SetBufferSize(80, 25);
 
-            }
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');
-            VerticalLine rightLine = new VerticalLine(78, 0, 24, '+');
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
-            Point p = new Point(4, 5, '*');
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+            Point p = new Point(20, 10, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
@@ -36,6 +24,10 @@ namespace Snake
             food.Draw();
             while (true)
             {
+                if ( walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
---
+++ Snake/Snake/Walls.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snake
+{
+    class Walls
+    {
+        List<Figure> wallsList;
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallsList = new List<Figure>();
+            HorizontalLine upLine = new HorizontalLine(0, mapWidth - 2, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, mapWidth-2, mapHeight - 2, '+');
+            VerticalLine leftLine = new VerticalLine(0, 0, mapHeight - 2, '+');
+            VerticalLine rightLine = new VerticalLine(mapWidth - 2, 0, mapHeight - 2, '+');
+            wallsList.Add(upLine);
+            wallsList.Add(downLine);
+            wallsList.Add(leftLine);
+            wallsList.Add(rightLine);
+        }
+
+        internal void Draw()
+        {
+            foreach(var wall in wallsList)
+            {
+                wall.Draw();
+            }
+        }
+
+        internal bool IsHit(Figure figure)
+        {
+            foreach(var wall in wallsList)
+            {
+                if(wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
